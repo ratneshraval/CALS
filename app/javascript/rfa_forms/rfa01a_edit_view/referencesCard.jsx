cc5227e898d16d5b7af5bffd0b94b2c9d884507e
anuroop <anuroop.buggaveeti15@gmail.com>
2017-09-01 10:16:42 -0700

    eslint errors cleared

--- app/javascript/rfa_forms/rfa01a_edit_view/referencesCard.jsx
+++ app/javascript/rfa_forms/rfa01a_edit_view/referencesCard.jsx
@@ -4,10 +4,10 @@ import CompleteNameFields from './completeNameField'
 import CommonAddressFields from 'components/rfa_forms/commonAddressField'
 
 export default class ReferencesCard extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
   }
-  render() {
+  render () {
     return (
       <div>
         <CompleteNameFields
@@ -20,13 +20,13 @@ export default class ReferencesCard extends React.Component {
           stateTypes={this.props.stateTypes}
           addressFields={this.props.referenceValues} />
         <InputComponent gridClassName='col-md-4' id='phone'
-                        value={this.props.phone_number}
-                        label='Phone' placeholder=''
-                        type='text' onChange={(event) => this.props.setParentState('street_address', event.target.value)} />
+          value={this.props.phone_number}
+          label='Phone' placeholder=''
+          type='text' onChange={(event) => this.props.setParentState('street_address', event.target.value)} />
         <InputComponent gridClassName='col-md-4' id='email'
-                        value={this.props.email}
-                        label='Email (optional)' placeholder=''
-                        type='text' onChange={(event) => this.props.setParentState('street_address', event.target.value)} />
+          value={this.props.email}
+          label='Email (optional)' placeholder=''
+          type='text' onChange={(event) => this.props.setParentState('street_address', event.target.value)} />
       </div>
     )
   }
--- app/javascript/rfa_forms/rfa01a_edit_view/referencesMain.jsx
+++ app/javascript/rfa_forms/rfa01a_edit_view/referencesMain.jsx
@@ -27,22 +27,22 @@ const blankAddressFields = Object.freeze({
     state: null
   }
 })
-export default class ReferenceMain extends React.Component{
-  constructor(props) {
+export default class ReferenceMain extends React.Component {
+  constructor (props) {
     super(props)
   }
   getFocusClassName (componentName) {
     return this.props.focusComponentName === componentName ? 'edit' : 'show'
   }
 
-  render() {
+  render () {
     const referenceValues = blankReferenceFields
     return (
       <div className="reference_main">
         <div>Please list the name, telephone number(s), and address of three individuals who have knowledge of your home environment, lifestyle, and
           ability to be a Resource Family. <b>*Must include 3 references</b></div>
         <div id='referenceMain_1' onClick={() => this.props.setFocusState('referenceMain_1')}
-                                 className={this.getFocusClassName('referenceMain_1') + ' ' + 'card reference-section double-gap-top'}>
+          className={this.getFocusClassName('referenceMain_1') + ' ' + 'card reference-section double-gap-top'}>
           <div className="card-header">
             <span>Reference -1</span>
           </div>
@@ -59,7 +59,7 @@ export default class ReferenceMain extends React.Component{
           </div>
         </div>
         <div id='referenceMain_2' onClick={() => this.props.setFocusState('referenceMain_2')}
-             className={this.getFocusClassName('referenceMain_2') + ' ' + 'card reference-section double-gap-top'}>
+          className={this.getFocusClassName('referenceMain_2') + ' ' + 'card reference-section double-gap-top'}>
           <div className="card-header">
             <span>Reference -2</span>
           </div>
@@ -76,7 +76,7 @@ export default class ReferenceMain extends React.Component{
           </div>
         </div>
         <div id='referenceMain_3' onClick={() => this.props.setFocusState('referenceMain_3')}
-             className={this.getFocusClassName('referenceMain_3') + ' ' + 'card reference-section double-gap-top'}>
+          className={this.getFocusClassName('referenceMain_3') + ' ' + 'card reference-section double-gap-top'}>
           <div className="card-header">
             <span>Reference -3</span>
           </div>
