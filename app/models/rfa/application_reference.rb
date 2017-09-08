11e3a691e7ce4ba972485ad464a5996bb0c12386
Lavanya Reddy <lreddy@cambriasolutions.com>
2017-09-08 08:56:25 -0700

    remove unnecessary attributes on references model

--- app/models/rfa/application_reference.rb
+++ app/models/rfa/application_reference.rb
@@ -1,9 +1,7 @@
 class Rfa::ApplicationReference < CalsBase
   include Concerns::Rfa::ApplicationReferenceApiProtocolProvider
 
-  attr_accessor :first_name, :middle_name,:last_name,
-                :phone_number, :email, :state, :street_address, :zip, :city,:state,
-                :name_suffix, :name_prefix, :validation_details, :references
+  attr_accessor :references
 
   def self.parent_path
     'rfa-1a-forms'
