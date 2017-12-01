bdf59421e8e976bdd717497c5663df5c053a0ebb
Sunil Biradar <sunilbiradar@Cambrias-MacBook-Pro.local>
2017-12-01 14:41:38 -0800

    fixedLintErrors

--- test/javascript/components/rfa_forms/agencyComponentFields_tests.js
+++ test/javascript/components/rfa_forms/agencyComponentFields_tests.js
@@ -2,57 +2,48 @@ import React from 'react'
 import {AgencyComponent} from 'components/rfa_forms/agencyComponentFields.js'
 import {shallow, mount} from 'enzyme'
 
-
-
 describe('verify agency component fields ', () => {
   let agencyComponent, onAgencyChangeSpy,
-  removeAgencyCardSpy, q1History, optionList;
+    removeAgencyCardSpy, q1History, optionList
 
-  optionList = ['a','b','c'];
+  optionList = ['a', 'b', 'c']
 
   beforeEach(() => {
     q1History = {
       'agencies': ['new']
     }
-    
 
     removeAgencyCardSpy = jasmine.createSpy('removeAgencyCard')
     onAgencyChangeSpy = jasmine.createSpy('onAgencyChange')
 
-
     agencyComponent = shallow(<AgencyComponent
-        index={0}
-	    id={'Q1-' + 0}
-	    defKey='foster_care_licenses_q1' 
-	    subKey='agencies'
-	    agencies={q1History.agencies}
-	    label='Agency Name'
-	    placeholder=''
-	    dropdownLabel='license type'
-	    inputId={'agency-q1-name-' + 0}
-	    dropDownId={'agency-q1-type-' + 0}
-	    optionList={optionList}
-	    dropDownValue='a'
-	    inputValue='ABC'
-	    removeAgencyCard={removeAgencyCardSpy}
-	    onAgencyChange={onAgencyChangeSpy}
+      index={0}
+      id={'Q1-' + 0}
+      defKey='foster_care_licenses_q1'
+      subKey='agencies'
+      agencies={q1History.agencies}
+      label='Agency Name'
+      placeholder=''
+      dropdownLabel='license type'
+      inputId={'agency-q1-name-' + 0}
+      dropDownId={'agency-q1-type-' + 0}
+      optionList={optionList}
+      dropDownValue='a'
+      inputValue='ABC'
+      removeAgencyCard={removeAgencyCardSpy}
+      onAgencyChange={onAgencyChangeSpy}
     />)
-
   })
 
-    
-    
   it('Test Render', () => {
     expect(agencyComponent.find('.list-item').length).toBe(1)
   })
-
 
   it('verify remove added Agency Name', () => {
     let removeButton = agencyComponent.find('span')
     removeButton.simulate('click', 'event')
     expect(removeAgencyCardSpy).toHaveBeenCalledWith('event', [ 'new' ], 0, 'foster_care_licenses_q1', 'agencies')
   })
-
 
   it('verify onChange function for agency name field', () => {
     let changeAgencyName = agencyComponent.find('#agency-q1-name-0')
@@ -63,11 +54,7 @@ describe('verify agency component fields ', () => {
   it('verify agency change drop down', () => {
     let agencyDropDownField = agencyComponent.find('#agency-q1-type-0')
     agencyDropDownField.simulate('change', {target: {selectedOptions: [{value: 1, text: 'License'}]}})
-    expect(onAgencyChangeSpy).toHaveBeenCalledWith( ({ target: ({ selectedOptions: [ ({ value: 1, text: 'License' }) ] }) }), 
-    [ 'new' ], 0, 'type', 'foster_care_licenses_q1', 'agencies', ({ id: 1, value: 'License' }) )
-    										
- 
+    expect(onAgencyChangeSpy).toHaveBeenCalledWith(({ target: ({ selectedOptions: [ ({ value: 1, text: 'License' }) ] }) }),
+      [ 'new' ], 0, 'type', 'foster_care_licenses_q1', 'agencies', ({ id: 1, value: 'License' }))
   })
-
-
 })
