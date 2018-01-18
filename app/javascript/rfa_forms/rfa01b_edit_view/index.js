66e3f7b320b9587c1c33aef909d20a07c82d7698
Nasar-cambria <nkprodware@gmail.com>
2018-01-17 16:34:24 -0800

    adding active-bar

--- app/javascript/rfa_forms/rfa01b_edit_view/index.js
+++ app/javascript/rfa_forms/rfa01b_edit_view/index.js
@@ -15,8 +15,7 @@ import {getDictionaryId, dictionaryNilSelect, checkArrayObjectPresence} from 'he
 import CardsGroupLayout from 'components/common/cardsGroupLayout.js'
 import {addCardAsJS, getFocusClassName, removeCard} from 'helpers/cardsHelper.jsx'
 import Validator from 'helpers/validator'
-
-//import './stylesheets/cards-01b.scss'
+import './stylesheets/cards-01b.scss'
 
 export default class Rfa01bList extends React.Component {
   constructor (props) {
